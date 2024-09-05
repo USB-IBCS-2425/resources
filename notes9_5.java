2ce2e6206b5a5e2e2778770f744f98adc7ea7c5c
Zack Considine <zack_considine@m018809.aw.org>
2024-09-05 14:04:38 -0700

    uploading notes

---
+++ notes9_5.java
@@ -0,0 +1,26 @@
+class notes9_5 {
+
+	public static void main(String[] args) {
+		
+		// Data types
+		// defined an integer variable with a value of 4
+		int x = 4;
+		// redefine the variable. set it equal to 6
+		x = 6;
+
+		// cast different types onto existing data
+		System.out.println(x);
+
+		float y = (float) x;
+
+		System.out.println(y);
+
+		// Strings are non-primitive and consist of characters
+		String favColor = "red";
+		System.out.println(favColor);
+
+		System.out.println(favColor + x);
+
+	}
+
+}
