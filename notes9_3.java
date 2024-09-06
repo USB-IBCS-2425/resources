01ee67e9570b1082393d98dd49842f9fe6d15bab
Zack Considine <zack_considine@M016196.aw.org>
2024-09-06 14:35:13 -0700

    added notes from day1

---
+++ notes9_3.java
@@ -0,0 +1,18 @@
+class notes9_3 { 
+	// main methods require the public
+	// key notation to allow access from outside
+	// the module
+	// static means that the method runs the same
+	// way every time it is accessed
+	// void is the return value
+	// nothing (void) is returned
+	// main is the name of the method
+	// String[] args is the parameter
+	// {} define the area for the main method
+	public static void main(String[] args) {
+		// this is inside (defining) the main
+		// method
+		System.out.print("Hello World!");
+	}
+
+}
