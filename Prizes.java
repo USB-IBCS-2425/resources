62f8755f67a7a83c82d1381ccb6b8e6bf793def1
Zack Considine <zack_considine@m018656.aw.org>
2024-09-17 13:19:22 -0700

    updating prizes

--- Prizes.java
+++ Prizes.java
@@ -4,34 +4,56 @@ class Prizes {
 	String prize3;
 	String prize4;
 	int numPrizes;
+	String mysteryPrize;
+
+	public void changePrize(String p, int n){
+		if (n == 1) {
+			this.prize1 = p;
+		}
+		if (n == 2) {
+			this.prize2 = p;
+		}
+		if (n == 3) {
+			this.prize3 = p;
+		}
+		if (n == 4) {
+			this.prize4 = p;
+		}
+	}
 
 	public String getNextPrize() {
 		String p = "";
-		if (numPrizes == 4) {
-			p = prize4;
+		if (this.numPrizes == 4) {
+			p = this.prize4;
 		}
-		else if (numPrizes == 3) {
-			p = prize3;
+		else if (this.numPrizes == 3) {
+			p = this.prize3;
 		}
-		else if (numPrizes == 2) {
-			p = prize2;
+		else if (this.numPrizes == 2) {
+			p = this.prize2;
 		}
-		else if (numPrizes == 1) {
-			p = prize1;
+		else if (this.numPrizes == 1) {
+			p = this.prize1;
 		}
 		else {
 			p = "There are no more prizes";
 		}
-		numPrizes = numPrizes - 1;
+		this.numPrizes = this.numPrizes - 1;
 		return p;
 	}
 
-	public Prizes() {
-		prize1 = "Civies Pass";
-		prize2 = "Mystery Box";
-		prize3 = "Mr. Stubb's brings cookies";
-		prize4 = "Pizza Party with Mr. Guadnola";
-		numPrizes = 4;
+	public String getMystery() {
+		return this.mysteryPrize;
+	}
+
+	public Prizes(String m) {
+		this.prize1 = "Civies Pass";
+		this.prize2 = "Mystery Box";
+		this.prize3 = "Mr. Stubb's brings cookies";
+		this.prize4 = "Pizza Party with Mr. Guadnola";
+		this.numPrizes = 4;
+		this.mysteryPrize = m;
+
 	}
 
 }
