c795e2cdf5cb77ac68fc3931088be8bf38eac357
Zack Considine <zack_considine@m016439.aw.org>
2024-10-01 11:59:17 -0700

    Practice Skeleton Code

---
+++ QuizPractice.java
@@ -0,0 +1,28 @@
+
+class QuizPractice {
+
+	// Q1: Find the average value of an array
+	// of doubles
+	public static double avgDoub(double[] d) {
+
+	}
+
+	// Q2: Find the longest word in an array
+	// of strings
+	public static String longestWord(String[] s) {
+		
+	}
+
+	// Q3: Determine the most average word length
+	// of a string (sentence)
+	public static int avgWord(String s) {
+
+	}
+	public static void main(String[] args) {
+	
+	}
+}
+
+
+
+
